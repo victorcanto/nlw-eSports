a303f5ea0eb237626644152436cb76518920158b
Victor Canto <iamvictorcanto@gmail.com>
2022-09-18 21:56:58 -0300

    feat(web): create Input component

---
+++ web/src/components/form/input/input.component.tsx
@@ -0,0 +1,15 @@
+import { InputHTMLAttributes } from 'react';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+export const Input = (props: InputProps) => {
+  return (
+    <input
+      id='game'
+      type='text'
+      placeholder='Selecione o game que deseja jogar'
+      className='bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500'
+      {...props}
+    />
+  );
+};
---
+++ web/src/components/index.ts
@@ -0,0 +1,3 @@
+export * from './create-ad-banner/create-ad-banner.component';
+export * from './game-banner/game-banner.component';
+export * from './form';
